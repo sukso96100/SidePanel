7c47179aaddb56fbb65516a2b83ce1cde850c043
sukso96100 <sukso96100@gmail.com>
2013-02-07 14:54:45 +0900

    Edited Layout

--- SidePanel/gen/kr/hybdms/sidepanel/R.java
+++ SidePanel/gen/kr/hybdms/sidepanel/R.java
@@ -960,16 +960,19 @@ containing a value of this type.
         public static final int abs__title=0x7f04001b;
         public static final int abs__titleDivider=0x7f04001c;
         public static final int abs__up=0x7f04000b;
+        public static final int button1=0x7f040035;
         public static final int disableHome=0x7f040009;
         public static final int edit_query=0x7f040027;
         public static final int homeAsUp=0x7f040006;
+        public static final int imageView1=0x7f040033;
         public static final int listMode=0x7f040002;
-        public static final int menu_settings=0x7f040033;
+        public static final int menu_settings=0x7f040036;
         public static final int normal=0x7f040001;
         public static final int showCustom=0x7f040008;
         public static final int showHome=0x7f040005;
         public static final int showTitle=0x7f040007;
         public static final int tabMode=0x7f040003;
+        public static final int toggleButton1=0x7f040034;
         public static final int useLogo=0x7f040004;
         public static final int wrap_content=0x7f040000;
     }
