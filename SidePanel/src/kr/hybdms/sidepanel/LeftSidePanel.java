c0d7a7656158a2d3c0b785378741aa4d963446d4
sukso96100 <sukso96100@gmail.com>
2013-02-12 22:51:00 +0900

    Added Custom ListView and related codes

--- SidePanel/src/kr/hybdms/sidepanel/LeftSidePanel.java
+++ SidePanel/src/kr/hybdms/sidepanel/LeftSidePanel.java
@@ -1,6 +1,9 @@
 package kr.hybdms.sidepanel;
 
 
+import java.util.ArrayList;
+import java.util.List;
+
 import kr.hybdms.sidepanel.util.SystemUiHider;
 
 import android.annotation.TargetApi;
@@ -9,10 +12,15 @@ import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.Gravity;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
 import android.widget.Button;
+import android.widget.ListView;
+import android.widget.Toast;
 import android.widget.ToggleButton;
 
 /**
@@ -21,11 +29,42 @@ import android.widget.ToggleButton;
  * 
  * @see SystemUiHider
  */
-public class LeftSidePanel extends Activity {
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		
-		setContentView(R.layout.activity_left_side_panel);
-	}
+public class LeftSidePanel extends Activity implements
+OnItemClickListener {
+	 
+public static final String[] titles = new String[] { "SidePanel"};
 
+public static final Integer[] images = { R.drawable.ic_launcher};
+
+ListView listView;
+List<PanelRowItem> rowItems;
+
+/** Called when the activity is first created. */
+@Override
+public void onCreate(Bundle savedInstanceState) {
+super.onCreate(savedInstanceState);
+setContentView(R.layout.activity_left_side_panel);
+
+rowItems = new ArrayList<PanelRowItem>();
+for (int i = 0; i < titles.length; i++) {
+    PanelRowItem item = new PanelRowItem(images[i], titles[i]);
+    rowItems.add(item);
+}
+
+listView = (ListView) findViewById(R.id.panelcontents);
+PanelArrayAdapter adapter = new PanelArrayAdapter(this,
+        R.layout.panelrow, rowItems);
+listView.setAdapter(adapter);
+listView.setOnItemClickListener(this);
+}
+
+@Override
+public void onItemClick(AdapterView<?> parent, View view, int position,
+    long id) {
+Toast toast = Toast.makeText(getApplicationContext(),
+    "Item " + (position + 1) + ": " + rowItems.get(position),
+    Toast.LENGTH_SHORT);
+toast.setGravity(Gravity.BOTTOM|Gravity.CENTER_HORIZONTAL, 0, 0);
+toast.show();
+}
 }
--- SidePanel/src/kr/hybdms/sidepanel/PanelArrayAdapter.java
+++ SidePanel/src/kr/hybdms/sidepanel/PanelArrayAdapter.java
@@ -1,13 +1,54 @@
 package kr.hybdms.sidepanel;
 
 import java.util.ArrayList;
+import java.util.List;
 
+import android.app.Activity;
 import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.BaseAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
+import kr.hybdms.sidepanel.PanelRowItem;
+import android.content.Context;
 
-public class PanelArrayAdapter {
-
+public class PanelArrayAdapter extends ArrayAdapter<PanelRowItem> {
+ 
+    Context context;
+ 
+    public PanelArrayAdapter(Context context, int resourceId,
+            List<PanelRowItem> items) {
+        super(context, resourceId, items);
+        this.context = context;
+    }
+ 
+    /*private view holder class*/
+    private class ViewHolder {
+        ImageView imageAppicon;
+        TextView txtApplable;
+    }
+ 
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ViewHolder holder = null;
+        PanelRowItem rowItem = getItem(position);
+ 
+        LayoutInflater mInflater = (LayoutInflater) context
+                .getSystemService(Activity.LAYOUT_INFLATER_SERVICE);
+        if (convertView == null) {
+            convertView = mInflater.inflate(R.layout.panelrow, null);
+            holder = new ViewHolder();
+            holder.txtApplable = (TextView) convertView.findViewById(R.id.appnametext);
+            holder.imageAppicon = (ImageView) convertView.findViewById(R.id.appicon);
+            convertView.setTag(holder);
+        } else
+            holder = (ViewHolder) convertView.getTag();
+ 
+        holder.txtApplable.setText(rowItem.getApp_lable());
+        holder.imageAppicon.setImageResource(rowItem.getApp_icon());
+ 
+        return convertView;
+    }
 }
