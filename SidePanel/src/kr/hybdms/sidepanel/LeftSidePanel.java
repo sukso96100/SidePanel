851f9981b211c04a02121106adbf6534d52ba269
sukso96100 <sukso96100@gmail.com>
2013-02-21 17:37:43 +0900

    Added Panel Click Event

--- SidePanel/src/kr/hybdms/sidepanel/LeftSidePanel.java
+++ SidePanel/src/kr/hybdms/sidepanel/LeftSidePanel.java
@@ -13,16 +13,17 @@ import kr.hybdms.sidepanel.util.SystemUiHider;
 
 import android.app.Activity;
 import android.app.ActivityManager;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.content.pm.PackageManager.NameNotFoundException;
 
 import android.os.Bundle;
-import android.view.Gravity;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ImageView;
 import android.widget.ListView;
-import android.widget.Toast;
 
 /**
  * An example full-screen activity that shows and hides the system UI (i.e.
@@ -30,44 +31,59 @@ import android.widget.Toast;
  * 
  * @see SystemUiHider
  */
-public class LeftSidePanel extends Activity implements
-OnItemClickListener {
-ListView listView;
-List<PanelItemDetail> rowItems;
-/** Called when the activity is first created. */
-@Override
-public void onCreate(Bundle savedInstanceState) {
-super.onCreate(savedInstanceState);
-setContentView(R.layout.activity_left_side_panel);
-
-listView = (ListView) findViewById(R.id.panelcontents);
-ActivityManager am = (ActivityManager) this.getSystemService(ACTIVITY_SERVICE);
-List<ActivityManager.RunningTaskInfo> tasks = am.getRunningTasks(30);
-rowItems = new ArrayList<PanelItemDetail>();
-PackageManager pacMgr = getPackageManager();
-  for (ActivityManager.RunningTaskInfo runningTask: tasks)
-  {
-    try {
-    	rowItems.add(new PanelItemDetail(pacMgr.getApplicationIcon(runningTask.topActivity.getPackageName())));
-    } catch (NameNotFoundException e) {
-      e.printStackTrace();
-    }
-  }
-
-listView = (ListView) findViewById(R.id.panelcontents);
-PanelArrayAdapter adapter = new PanelArrayAdapter(this,
-        R.layout.panelrow, rowItems);
-listView.setAdapter(adapter);
-listView.setOnItemClickListener(this);
-}
-
-@Override
-public void onItemClick(AdapterView<?> parent, View view, int position,
-    long id) {
-Toast toast = Toast.makeText(getApplicationContext(),
-    "Item " + (position + 1) + ": " + rowItems.get(position),
-    Toast.LENGTH_SHORT);
-toast.setGravity(Gravity.BOTTOM|Gravity.CENTER_HORIZONTAL, 0, 0);
-toast.show();
-}
+public class LeftSidePanel extends Activity {
+	 private ArrayList<PanelItemDetail> rowItems = null;
+	 private ListView listView;
+	 private ArrayList<String> packageName = null;
+	 private ArrayList<String> className = null;
+	 
+	 protected void onCreate(Bundle savedInstanceState) {
+	  super.onCreate(savedInstanceState);
+	  setContentView(R.layout.activity_left_side_panel);
+	  
+	  ImageView imgbtn = (ImageView) findViewById(R.id.transparentbackground);
+	  listView = (ListView)findViewById(R.id.panelcontents);
+	  packageName = new ArrayList<String>();
+	  className = new ArrayList<String>();
+	  
+	  
+	  ActivityManager am = (ActivityManager) this.getSystemService(ACTIVITY_SERVICE);
+	  List<ActivityManager.RunningTaskInfo> tasks = am.getRunningTasks(30);
+	  rowItems = new ArrayList<PanelItemDetail>();
+	  PackageManager pacMgr = getPackageManager();
+	  
+	  
+	  for (ActivityManager.RunningTaskInfo runningTask : tasks) {
+	   try {
+	    rowItems.add(new PanelItemDetail(pacMgr.getApplicationIcon(runningTask.topActivity.getPackageName())));
+	    packageName.add(runningTask.topActivity.getPackageName());
+	    className.add(runningTask.topActivity.getClassName());
+	    
+	    Log.d("#@#", "getPackageName = " + runningTask.topActivity.getPackageName());
+	    Log.d("#@#", "getClassName = " + runningTask.topActivity.getClassName());
+	   } catch (NameNotFoundException e) {
+	    e.printStackTrace();
+	   }
+	  }
+	  
+	  
+	  PanelArrayAdapter adapter = new PanelArrayAdapter(this,R.layout.panelrow, rowItems);
+	  listView.setAdapter(adapter);
+	  listView.setOnItemClickListener(new OnItemClickListener() {
+	   public void onItemClick(AdapterView<?> parent, View view, int postion, long id) {
+	       startActivity(new Intent().setClassName(packageName.get(postion).toString(), className.get(postion).toString()).addFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT));
+	       finish();
+	   } 
+	  });
+	  imgbtn.setOnClickListener(new View.OnClickListener(){
+			 public void onClick(View v) {
+				 if(v.getId() ==R.id.transparentbackground){
+					 moveTaskToBack(true);
+						finish();
+				 }
+			 } 
+		  });
+	  
+	  
+	 }
 }
