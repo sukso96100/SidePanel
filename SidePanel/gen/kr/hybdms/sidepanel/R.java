a50baad0b042aaf42d7a614784e40c3aec661924
sukso96100 <sukso96100@gmail.com>
2013-03-10 17:01:41 +0900

    Updated Changes

--- SidePanel/gen/kr/hybdms/sidepanel/R.java
+++ SidePanel/gen/kr/hybdms/sidepanel/R.java
@@ -1168,6 +1168,21 @@ containing a value of this type.
          */
         public static final int abs__shareactionprovider_share_with_application=0x7f0c0009;
         public static final int action_settings=0x7f0c001d;
+        /**  Copyright 2013 Young Bin Han
+
+     Licensed under the Apache License, Version 2.0 (the "License");
+     you may not use this file except in compliance with the License.
+     You may obtain a copy of the License at
+
+          http://www.apache.org/licenses/LICENSE-2.0
+
+     Unless required by applicable law or agreed to in writing, software
+     distributed under the License is distributed on an "AS IS" BASIS,
+     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+     See the License for the specific language governing permissions and
+     limitations under the License.
+
+         */
         public static final int app_name=0x7f0c000f;
         public static final int button_appinfo=0x7f0c0010;
         public static final int dummy_button=0x7f0c0013;
